f06e6db4b6dcd2d822362a5274cd9b438127f27e
capmahin <mahinulhuque1996@gmail.com>
2022-06-09 18:20:48 +0600

    Finish

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -3,7 +3,19 @@ import React from "react";
 const About = () => {
   return (
     <div>
-      <h1>About</h1>
+      <h1 className="text-3xl font-bold text-purple-600 text-center">About</h1>
+      <p className="text-xl font-bold">
+        {" "}
+        I can build Full Stack website. i like to build Front-End and Back-end
+        both side .. i love to work using React. i work many MEAN Stack
+        Projects. I’ve always been someone who has both a creative and a logical
+        side. When I discovered web design. i love to Prefer wroking in MEAN
+        Stack Project. A full stack web developer is a person who can develop
+        both client and server software.In addition to mastering HTML and CSS,
+        he knows how to: Program a browser (like using JavaScript, jQuery,
+        Angular, or Vue) Program a server (like using PHP, ASP, Python, or Node)
+        Program a database (like using SQL, SQLite, or MongoDB).
+      </p>
     </div>
   );
 };
--- src/Pages/Home/Projects.js
+++ src/Pages/Home/Projects.js
@@ -21,6 +21,11 @@ const Projects = () => {
           server software.In addition to mastering HTML and CSS, he knows how
           to: Program a browser (like using JavaScript, jQuery, Angular, or Vue)
           Program a server (like using PHP, ASP, Python, or Node) Program a
+          database (like using SQL, SQLite, or MongoDB). A full stack web
+          developer is a person who can develop both client and server
+          software.In addition to mastering HTML and CSS, he knows how to:
+          Program a browser (like using JavaScript, jQuery, Angular, or Vue)
+          Program a server (like using PHP, ASP, Python, or Node) Program a
           database (like using SQL, SQLite, or MongoDB).
         </p>
         <div>
